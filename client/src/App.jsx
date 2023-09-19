223326df82b4764edb1985f93e4f65a9114cefe8
vishnuarjun512 <vishnuarjun512@gmail.com>
2023-09-19 15:11:44 +0530

    Routing changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,34 @@
-import React from 'react'
-import './App.scss'
-import Dashboard from './components/Dashboard/dashboard'
-import Login from './components/Login/login'
-import Register from './components/Register/register'
+import React from "react";
+import "./App.scss";
+import Dashboard from "./components/Dashboard/dashboard";
+import Login from "./components/Login/login";
+import Register from "./components/Register/register";
 
 //Import React react Router dom
-import {
-  createBrowserRouter,
-  RouterProvider
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 //Creating a Router
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <div> <Login/></div>
+    path: "/",
+    element: <Login />,
   },
   {
-    path: '/register',
-    element: <div> <Register/></div>
+    path: "/register",
+    element: <Register />,
   },
   {
-    path: '/dashboard',
-    element: <div> <Dashboard/></div>
-  }
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
 ]);
 
 function App() {
   return (
     <div>
-     <RouterProvider router={router}/> 
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
